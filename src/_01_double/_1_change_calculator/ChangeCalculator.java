d204284ab1741774e9846acbae4d40f54bc7645b
Meme1sh_Fetish <60296686+The-GitHub-Paradox@users.noreply.github.com>
2020-06-17 14:26:58 -0700

    e
    e

--- src/_01_double/_1_change_calculator/ChangeCalculator.java
+++ src/_01_double/_1_change_calculator/ChangeCalculator.java
@@ -12,15 +12,17 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String input = JOptionPane.showInputDialog("How many Nickels do you have?");
 		// Convert their answer to an int.   Hint: Integer.parseInt()  
-
+int Nickelnum = Integer.parseInt(input);
 		// Ask the user how many dimes they have, and convert their answer
-
+String input2 = JOptionPane.showInputDialog("How many Dimes do you have?");
+int Dimenum = Integer.parseInt(input2);
 		// Ask the user how many quarters they have, and convert their answer
-
+String input3 = JOptionPane.showInputDialog("How many Quarters do you have?");
+int Quarternum = Integer.parseInt(input3);
 		// Calculate how much money the user has.  Hint: Use a double variable 
-
+//System.out.print(input*0.05 + input2*0.1 + input3*0.25);
 		// Tell the user how much money they have in dollars and cents format (e.g. $6.75)
 
 	}
--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -12,9 +12,15 @@ public class SleepyHead {
 		/************************  SLEEPY HEAD  **************************/
 		boolean isWeekend;
 		// Write code to ask the user what day it is.
-        
+        String input = JOptionPane.showInputDialog("What day is it? Weekend(type 1) or not weekend(type 2)?");
+        int day = Integer.parseInt(input);
 		// Set the boolean isWeekend based on the value they enter
-		
+		if(day == 1) {
+			System.out.print("You get to sleep in!");
+		}
+		else {
+			System.out.println("Get out of bed and go to school!");
+		}
 		
 		// If it is the weekend, tell the user they get to sleep in.
 		
@@ -25,9 +31,15 @@ public class SleepyHead {
 
 		boolean passedExam;
 		// Write code to ask the user what percentage they scored in their last exam
-
-		
+String input2 = JOptionPane.showInputDialog("What percentage did you get on your test?");
+int test = Integer.parseInt(input2);
 		// If they scored more than 70, they passed the exam.
+if(test >= 70) {
+	System.out.print("   Good Job! You Passed!");
+}
+else {
+	System.out.print("   Ha noob");
+}
 		// Set the boolean passedExam based on their score.
 		
 		// If the user passed the exam, congratulate them
@@ -53,13 +65,20 @@ public class SleepyHead {
 		
 		/***********************   RED SQUARE  ********************/
 		
-		boolean isRed;
+		boolean isRed = false;
 		// Ask the user what color to draw with. Based on their answer, set the isRed variable
-		
-		
+		while(!isRed) {
+			String red = JOptionPane.showInputDialog("What color do you want to draw with?");
+			if(red.equalsIgnoreCase("red"));
+			isRed = true;
+		}
 		boolean isSquare;		
 		// Now ask the user what shape to draw. Based on their answer, set the isSquare variable
-		
+		while(!isSquare) {
+			String square = JOptionPane.showInputDialog("What shape do you want to draw?");
+			if(square.equalsIgnoreCase("square"));
+			isSquare = true;
+		}
 		
 		// Now you MUST use the && operator to join the booleans for this code. 
 		// Ask your teacher if you are not sure how
